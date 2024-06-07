fe34b206a9f0af61b4f54d48232098666417e3c0
HiKKors <133841591+HiKKors@users.noreply.github.com>
2024-06-07 23:12:32 +0400

    Add files via upload

--- index_telegraf.js
+++ index_telegraf.js
@@ -1,28 +1,28 @@
-import { Telegraf, Markup } from 'telegraf'
-import { message } from 'telegraf/filters'
+import { Telegraf, Markup } from 'telegraf';
+import { message } from 'telegraf/filters';
 import { db, User } from './db.js';
-
+import express from 'express';
+import bodyParser from 'body-parser';
 
 // импорт базы данных
 // const User = require('./db').User;
 
-const token = '6839956141:AAEHaAlbHk-m5VFPFkrJDXflekn5AHR6inM'
-const webAppUrl = 'https://vk.com/'
+const token = '6839956141:AAEHaAlbHk-m5VFPFkrJDXflekn5AHR6inM';
+const webAppUrl = 'https://vk.com/';
 
-const MOVIE_API = 'W9YK7TA-SXQ4V4E-KXR6NS8-XF8K78Q'
+const MOVIE_API = 'W9YK7TA-SXQ4V4E-KXR6NS8-XF8K78Q';
 
-// const MOVIE_URL_QUERY = 'https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=10&query=%D1%8F%20%D0%BB%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0'
+// const MOVIE_URL_QUERY = 'https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=10&query=%D1%8F%20%D0%BB%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0';
 
-const test_url = 'https://my-json-server.typicode.com/typicode/demo/db'
+const test_url = 'https://my-json-server.typicode.com/typicode/demo/db';
 
-const bot = new Telegraf(token)
-
-
+const bot = new Telegraf(token);
 
 function is_superuser(username) {
-  let status = User.get_user_status(username)
-  return status
+  let status = User.get_user_status(username);
+  return status;
 }
+
 // поиск фильма по названию
 bot.command('start', (ctx) => {
   ctx.reply(
@@ -30,10 +30,10 @@ bot.command('start', (ctx) => {
     Markup.keyboard([
       Markup.button.webApp('Отправить сообщение', `${webAppUrl}/feedback`),
     ])
-  )
-})
+  );
+});
 
-bot.command('register',async (ctx) => {
+bot.command('register', async (ctx) => {
   const { id, first_name, last_name, username } = ctx.from;
     
   const userData = {
@@ -54,7 +54,7 @@ bot.command('register',async (ctx) => {
       }
     }
   });
-})
+});
 
 bot.command('test', async (ctx) => {
   const data = await fetch('https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=10&query=дорога+перемен',{
@@ -65,8 +65,8 @@ bot.command('test', async (ctx) => {
   });
   const respData = await data.json();
   // ctx.reply(JSON.stringify(respData.docs[0].description))
-  ctx.reply(username)
-})
+  ctx.reply(username);
+});
 
 // топ фильмов
 bot.command('top', async (ctx) => {
@@ -79,18 +79,18 @@ bot.command('top', async (ctx) => {
   const respData = await data.json();
   // let movie_info = JSON.stringify(respData.docs[0])
   let movie_info = respData.docs[0];
-  let movieLength = parseInt(JSON.stringify(movie_info.movieLength))
+  let movieLength = parseInt(JSON.stringify(movie_info.movieLength));
   let string_info = `Рейтинг: ${JSON.stringify(movie_info.rating.kp)}
 Название: ${JSON.stringify(movie_info.name)}
 Год: ${JSON.stringify(movie_info.year)}
 Жанр: ${JSON.stringify(movie_info.genres)}
 Длительность: ${Math.round(movieLength / 60)} ч ${movieLength % 60} мин
 Возрастная группа: ${JSON.stringify(movie_info.ageRating)}+
-Описание: ${JSON.stringify(movie_info.description)}`
+Описание: ${JSON.stringify(movie_info.description)}`;
   for (let i = 0; i < 5; i++){
-    ctx.reply(string_info)
+    ctx.reply(string_info);
   }
-})
+});
 
 bot.command('help', async (ctx) => {
   const username = ctx.from.username;
@@ -115,7 +115,6 @@ bot.command('help', async (ctx) => {
     ctx.reply(info);
   });
 });
-
 
 bot.command('users', async (ctx) => {
   const username = ctx.from.username;
@@ -149,7 +148,6 @@ bot.command('users', async (ctx) => {
     }
   });
 });
-
 
 bot.command('setstatus', async (ctx) => {
   const username = ctx.from.username;
@@ -242,5 +240,36 @@ bot.action(/set_status_(.+)/, async (ctx) => {
   });
 });
 
+// Создание сервера Express для приема данных из веб-приложения
+const app = express();
+app.use(bodyParser.json());
 
-bot.launch()
+app.post('/sendMovie', async (req, res) => {
+  const movie = req.body;
+
+  const chatId = 'YOUR_CHAT_ID'; // Замените на реальный chat ID или динамически получайте его
+
+  const message = `
+Название: ${movie.nameRu || movie.nameEn}
+Рейтинг: ${movie.rating || movie.ratingKinopoisk || movie.ratingImdb}
+Жанр: ${movie.genres.map(genre => genre.genre).join(', ')}
+Год: ${movie.year}
+Описание: ${movie.description}
+Время: ${movie.filmLength || 'Не указано'} минут
+Сайт: ${movie.webUrl || 'Не указан'}
+`;
+
+  try {
+    await bot.telegram.sendPhoto(chatId, { url: movie.posterUrl }, { caption: message });
+    res.status(200).send('Message sent');
+  } catch (error) {
+    console.error('Error sending message to Telegram', error);
+    res.status(500).send('Failed to send message');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+bot.launch();
